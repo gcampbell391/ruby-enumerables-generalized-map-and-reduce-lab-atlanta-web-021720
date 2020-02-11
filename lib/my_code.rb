3387db50476341a133a83527bbef793c34a7e523
Gene Campbell III <gcampbell391@gmail.com>
2020-02-11 18:53:58 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -9,5 +9,15 @@
  end 
 
 def reduce(array, starting_value = nil)
-  
+  if starting_value 
+    sum = starting_value
+    i = 0
+  else 
+    sum = array[0]
+    i = 1 
+  end 
+  while i < array.length 
+  sum = yield(sum, array[i])
+  i += 1
+end 
 end 
